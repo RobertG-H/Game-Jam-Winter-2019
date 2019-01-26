27090dc22a924bd81524d259ff98349fa18d072e
Daniel-U12 <daniel.utvich12@gmail.com>
2019-01-25 22:30:29 -0500

    cummy daddy

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = target.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate() {
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+        transform.LookAt(target.transform);
+    }
+}
