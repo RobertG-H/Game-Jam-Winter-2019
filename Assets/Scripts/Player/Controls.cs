266aac95dcbc366e373ea70fa4bf0f968c6e9800
Daniel-U12 <daniel.utvich12@gmail.com>
2019-01-26 11:08:10 -0500

    getting da anim

--- Assets/Scripts/Player/Controls.cs
+++ Assets/Scripts/Player/Controls.cs
@@ -3,29 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controls : MonoBehaviour {
-    public float movementSpeed = 30;
-    public float acceleration = 10;
+    public float startSpeed = 10;
+    public float movementSpeed;
+    public float MAX_SPEED = 20;
+    public float acceleration = 0.1f;
+
     public float turningSpeed = 200;
     float time = 0;
     public Rigidbody rb;
 
     void start() {
         rb = GetComponent<Rigidbody>();
+        movementSpeed = startSpeed;
     }
     void Update() {
         float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
         transform.Rotate(0, horizontal, 0);
-        
+
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
         if ( Input.GetKey(KeyCode.W) ) {
-            
-            Debug.Log("velocity: " + rb.velocity.magnitude);
-            float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime * time;
-            transform.Translate(0, 0, vertical);
-            if ( rb.velocity.magnitude < 1f/1000000000000000000000000f ) {
-                time += Time.deltaTime * acceleration;
+            if ( movementSpeed < MAX_SPEED ) {
+                movementSpeed += acceleration;
+                Debug.Log("speed: " + movementSpeed);
             }
-        } else if (Input.GetKeyUp(KeyCode.W)) {
-            time = 0;
+            transform.Translate(new Vector3(0, 0, vertical)); 
+        } else if ( Input.GetKey(KeyCode.S) ) {
+            transform.Translate(new Vector3(0,0, vertical));
+        }
+        if ( Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) ){
+            movementSpeed = startSpeed;
         }
         
     }   
@@ -33,4 +39,5 @@ public class Controls : MonoBehaviour {
     {
         
     }
+
 }
