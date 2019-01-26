44b9d73802a1e45ce834a10635875468a75d8731
Daniel-U12 <daniel.utvich12@gmail.com>
2019-01-26 14:44:50 -0500

    player can jump ish

--- Assets/Scripts/Player/Controls.cs
+++ Assets/Scripts/Player/Controls.cs
@@ -9,24 +9,21 @@ public class Controls : MonoBehaviour {
     public float acceleration = 0.1f;
 
     public float turningSpeed = 200;
+
+    private bool grounded = true;
     float time = 0;
-
-    public Animator animator;
-
+    public Rigidbody rb;
     void start() {
         movementSpeed = startSpeed;
-        animator =  GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
     void Update() {
         float horizontal = Input.GetAxis("Horizontal0") * turningSpeed * Time.deltaTime;
-        Debug.Log("horz:" + horizontal);
         transform.Rotate(0, horizontal, 0);
-        animator.SetFloat("Speed", 0);
         float vertical = Input.GetAxis("Vertical0") * movementSpeed * Time.deltaTime;
         if ( Input.GetKey(KeyCode.W) ) {
             if ( movementSpeed < MAX_SPEED ) {
                 movementSpeed += acceleration;
-                Debug.Log("speed: " + movementSpeed);
             }
             transform.Translate(new Vector3(0, 0, vertical)); 
         } else if ( Input.GetKey(KeyCode.S) ) {
@@ -35,8 +32,30 @@ public class Controls : MonoBehaviour {
         if ( Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) ){
             movementSpeed = startSpeed;
         }
+
+        if ( Input.GetKeyDown(KeyCode.Space) && grounded ) {
+            if ( rb.velocity.x == 0 )
+                rb.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
+            else {
+                rb.AddForce(new Vector3(0,12,1), ForceMode.Impulse);
+            }
+                
+
+        } else if ( !grounded ) {
+
+        }
         
     }   
+    void OnCollisionExit(Collision col ) {
+        if ( col.gameObject.tag == "Land" ) {
+            grounded = false;
+        }
+    }
+    void OnCollisionStay(Collision col){
+        if ( col.gameObject.tag == "Land" ) {
+            grounded = true;
+        }
+    }
     void FixedUpdate()
     {
         
