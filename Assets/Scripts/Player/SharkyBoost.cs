cb2d9dd34dff6701c339cb75ed482c63a02a191f
Daniel-U12 <daniel.utvich12@gmail.com>
2019-01-26 18:35:29 -0500

    shark prefeb, lol this is on music branch

---
+++ Assets/Scripts/Player/SharkyBoost.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkyBoost : MonoBehaviour
+{
+    
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.W)){
+            Debug.Log("is working?");
+            rb.AddForce(transform.forward * 20, ForceMode.Impulse);
+        }
+        
+    }
+}
